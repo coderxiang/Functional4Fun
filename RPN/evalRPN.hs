9b83abe198420e5d6ba181a45002197425a4d944
coderxiang <lebesgue@lebesgue.net>
2013-09-27 20:40:25 -0700

    RPN: Read type class; simplify code

--- RPN/evalRPN.hs
+++ RPN/evalRPN.hs
@@ -1,20 +1,6 @@
-isNum :: String -> Bool
-isNum s = s /= "*" && s /= "+" && s /= "-"
-
-eval :: (Num a) => String -> a -> a -> a
-eval op x y
-  | op == "*" = x * y
-  | op == "+" = x + y
-  | op == "-" = y - x
-
-evalPair :: [Double] -> String -> [Double]
-evalPair l op = 
-  if isNum op then ((read :: String->Double) op) : l
-  else 
-    case l of
-      x:y:ys -> (eval op x y) : ys
-      [] -> []
-    
-                               
-evalRPN ::  String -> [Double]
-evalRPN = foldl evalPair [] . words
+evalRPN ::  (Num a, Read a) => String -> [a]
+evalRPN = foldl evalOp [] . words
+          where evalOp (x:y:ys) "*" = (x*y) : ys
+                evalOp (x:y:ys) "+" = (x+y) : ys
+                evalOp (x:y:ys) "-" = (y-x) : ys
+                evalOp xs number = read number : xs
